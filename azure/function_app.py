fe6654a01bf2209edc5cf6c1c23214bf5890a74a
JongseoKang <kjs010602@naver.com>
2025-04-14 03:17:50 +0000

    azure test

--- azure/function_app.py
+++ azure/function_app.py
@@ -1,28 +1,27 @@
 import azure.functions as func
 import logging
+import requests
 from profile.runner import ModelRunner
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="test")
+def test(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(f"Test function is working.")
 
 @app.route(route="main")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    model = req.params.get('model')
-    if not model:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            model= req_body.get('model')
-    
-    if model:
-        pass
+    # Call the 'test' function
+    test_function_url = req.url.replace("main", "test")  # Adjust URL for 'test' function
+    response = requests.get(test_function_url)
+
+    return func.HttpResponse(f"Response from 'test': {response.text}")
         
 @app.route(route="resnet18")
 def resnet18(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('resnet18 function processed a request.')
 
     valid = req.params.get('valid')
     if not valid:
